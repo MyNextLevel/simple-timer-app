8393817a7178f207accbcdaf4f6a16366cdba73e
Donaboyev <donaboyevabbos1701@gmail.com>
2020-12-19 16:29:42 +0500

    04. Empty project. Only splash screen is added

--- app/src/main/java/com/abbasibnilkham/timerapp/fragments/AlarmFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.abbasibnilkham.timerapp.fragments
-
-import androidx.fragment.app.Fragment
-import com.abbasibnilkham.timerapp.R
-
-class AlarmFragment:Fragment(R.layout.fragment_alarm) {
-}
